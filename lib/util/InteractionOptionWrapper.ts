7dd4afdf899563e53fd8c97f2dacf6be24b45ca3
raphckrman <41128238+raphckrman@users.noreply.github.com>
2024-08-24 20:44:35 +0200

    fix(interaction): check pour rien sorry

--- lib/util/InteractionOptionWrapper.ts
+++ lib/util/InteractionOptionWrapper.ts
@@ -142,9 +142,7 @@ export class InteractionOptionWrapper {
         if (type === ApplicationCommandOptionType.BOOLEAN)
             this.values[optionIndex] = Boolean(this.values[optionIndex]);
         if (type === ApplicationCommandOptionType.EMOTE) {
-            if (typeof this.values[optionIndex] === "number") {
-                this.values[optionIndex] = Number(this.values[optionIndex]);
-            } else {
+            if (typeof this.values[optionIndex] !== "number") {
                 const emoteID = Number((this.values[optionIndex] as string)?.match(/<:\w+:(\d+)>/)?.[1]);
                 if (isNaN(emoteID)) return;
                 this.values[optionIndex] = Number(emoteID);
